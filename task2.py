af4d44edefd70975a20d4f384a094c9ff2794379
Vorobyov Kirill <vorobyovk@gmail.com>
2025-03-16 20:31:25 +0100

    task2

--- task2.py
+++ task2.py
@@ -2,19 +2,23 @@ import os
 
 PATH = 'cats.txt'
 
-def check_file_exists(path):
+def check_file_exists(path): # function to check if file exist
     return os.path.exists(path)
 
-def get_cats_info(path):
+def get_cats_info(path): # function to get cats info
     if not check_file_exists(path):
         return 'File does not exist'
     else:
         with open(path, 'r') as file:
             lines = file.readlines()
-            cats = []
+            cats = [] # list to store cats info
             for line in lines:
-                cat = line.strip().split()
-                cats.append(cat)
+                # Check if the line has 25 symbols and the third element is a digit
+                if line.split(',')[0].count('') == 25 and line.rstrip('\n').split(',')[2].isdigit() == True:
+                    cat = line.strip().split()
+                    cats.append(cat)
+                else:
+                    return 'Invalid data'
         return cats
-
+# Print the result
 print(get_cats_info(PATH))
