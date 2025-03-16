0f0d7e500bad91511afdc824fde507faa0c3066d
Vorobyov Kirill <vorobyovk@gmail.com>
2025-03-16 19:53:31 +0100

    task1

--- task1.py
+++ task1.py
@@ -1,21 +1,26 @@
+import os
 
-PATH = 'sallary.txt'
+PATH = 'sallary.txt' # path to file
 
-def read_file(path):    
-    with open(PATH, 'r') as file:
-        
+def is_file_exist(path):  # function to check if file exist  
+    if os.path.exists(path) == True:
+        return True
+    else:
+        return False
 
-
-def total_salary(path):
-    with open(path, 'r') as file:
-        lines = file.readlines()
-        index = 0
-        total = 0
-        avarage = 0
-        for line in lines:
-            total = total + int(line.split(',')[1])
-            index += 1
-        avarage = total / index
+def total_salary(path): # function to calculate total salary and avarage salary
+    if is_file_exist(path) == False:
+        return "File is not exist"
+    else:
+        with open(path, 'r') as file:
+            lines = file.readlines()
+            index = 0 # index to count number of employers
+            total = 0 # total salary
+            avarage = 0 # avarage salary
+            for line in lines:
+                total = total + int(line.split(',')[1])
+                index += 1
+            avarage = total / index
         return total, avarage
 
-print(total_salary(PATH))
+print(total_salary(PATH)) # print total salary and avarage salary
